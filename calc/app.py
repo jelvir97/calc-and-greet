dd04388d8cf16b4d3da01568e327d20f3ab86b00
Juan Elvir <jelvir97@gmail.com>
2023-05-16 16:50:12 -0600

    calc: handled add request

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,14 @@
 # Put your app in here.
+from flask import Flask, request
+from operations import *
+
+app = Flask(__name__)
+
+@app.route('/add')
+def add_request():
+    """Handle add request"""
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+
+    return f"{a} plus {b} equals {add(a,b)}"
+
