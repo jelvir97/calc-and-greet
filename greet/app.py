236695dbe3fbe1685f98e51048827b888c2a2c55
Juan Elvir <jelvir97@gmail.com>
2023-05-16 16:30:55 -0600

    greet: welcome back and welcome home requests

--- greet/app.py
+++ greet/app.py
@@ -7,3 +7,12 @@ def welcome():
     """Returns 'Welcome' from /welcome request"""
     return '<h1>Welcome</h1>'
 
+@app.route('/welcome/home')
+def welcome():
+    """Returns 'Welcome Home' from /welcome/home request"""
+    return '<h1>Welcome Home</h1>'
+
+@app.route('/welcome/back')
+def welcome():
+    """Returns 'Welcome back' from /welcome/back request"""
+    return '<h1>Welcome Back</h1>'
