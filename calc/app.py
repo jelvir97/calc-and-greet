69b61fa33bcbdd19ae6c1aa1ef0084c58dca7ca8
Juan Elvir <jelvir97@gmail.com>
2023-05-16 16:56:07 -0600

    calc: handled sub, mult, and div requests

--- calc/app.py
+++ calc/app.py
@@ -12,3 +12,26 @@ def add_request():
 
     return f"{a} plus {b} equals {add(a,b)}"
 
+@app.route('/sub')
+def sub_request():
+    """Handle subtract request"""
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+
+    return f"{a} minus {b} equals {sub(a,b)}"
+
+@app.route('/mult')
+def mult_request():
+    """Handle multiply request"""
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+
+    return f"{a} times {b} equals {mult(a,b)}"
+
+@app.route('/div')
+def div_request():
+    """Handle divide request"""
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+
+    return f"{a} divided by {b} equals {div(a,b)}"
