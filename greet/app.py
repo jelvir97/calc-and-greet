2529c993cb7d1ac1ff555388294b8dc33299a08b
Juan Elvir <jelvir97@gmail.com>
2023-05-16 16:32:28 -0600

    changed funciton names

--- greet/app.py
+++ greet/app.py
@@ -8,11 +8,11 @@ def welcome():
     return '<h1>Welcome</h1>'
 
 @app.route('/welcome/home')
-def welcome():
+def welcome_home():
     """Returns 'Welcome Home' from /welcome/home request"""
     return '<h1>Welcome Home</h1>'
 
 @app.route('/welcome/back')
-def welcome():
+def welcome_back():
     """Returns 'Welcome back' from /welcome/back request"""
     return '<h1>Welcome Back</h1>'
